5b29717e3410183b830129f5ab2ae001f5d30ae7
Avanishdubey1707 <avanishdubeymsd@gmail.com>
2026-01-12 05:10:05 +0530

    Created and fetched the Quiz

--- src/main/java/com/putin/quizapp/service/QuizService.java
+++ src/main/java/com/putin/quizapp/service/QuizService.java
@@ -1,14 +1,19 @@
 package com.putin.quizapp.service;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.putin.quizapp.Model.Question;
+import com.putin.quizapp.Model.QuestionWrapper;
 import com.putin.quizapp.Model.Quiz;
 import com.putin.quizapp.dao.QuestionDao;
 import com.putin.quizapp.dao.QuizDao;
@@ -36,5 +41,19 @@ public class QuizService {
         quizDao.save(quiz);
 
         return ResponseEntity.ok("Quiz created successfully");
+        
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+        Optional<Quiz> quiz=quizDao.findById(id);
+        List<Question> questionsFromDB=quiz.get().getQuestions();
+        List<QuestionWrapper> questionsForUser =  new ArrayList<>();
+        for(Question q: questionsFromDB){
+            QuestionWrapper qw=new QuestionWrapper(q.getId(),q.getQuestionTitle(), q.getOption1(),q.getOption2(),q.getOption3(),q.getOption4());
+            questionsForUser.add(qw);
+
+
+        }
+        return new ResponseEntity<>(questionsForUser,HttpStatus.OK);
     }
 }
